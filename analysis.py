beb02695ad67269eba0b837653b711ab48b8d10f
alzhwr795-ux <alzhwr795@gmail.com>
2026-07-19 16:09:45 +0300

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,12 +1,17 @@
 import requests
 
 def analyze_btc():
-    url = "https://fapi.binance.com/fapi/v1/ping"
+    url = "https://api.bybit.com/v5/market/tickers?category=linear&symbol=BTCUSDT"
 
     response = requests.get(url, timeout=10)
+    data = response.json()
+
+    price = float(data["result"]["list"][0]["lastPrice"])
+
+    signal = "🟢 LONG"
 
     return {
-        "signal": "🟢 LONG",
-        "price": response.status_code,
-        "ema": "OK"
+        "signal": signal,
+        "price": price,
+        "ema": "قيد التطوير"
     }
