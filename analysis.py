bc028c104f3d2a82930a457ea403f6b12a6fc095
alzhwr795-ux <alzhwr795@gmail.com>
2026-07-19 15:54:55 +0300

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,20 +1,6 @@
-import requests
-
 def analyze_btc():
-    url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
-
-    response = requests.get(url, timeout=10)
-    data = response.json()
-
-    price = float(data["price"])
-
-    if price > 100000:
-        signal = "🟢 LONG"
-    else:
-        signal = "🔴 SHORT"
-
     return {
-        "signal": signal,
-        "price": price,
-        "ema": "قيد الحساب"
+        "signal": "🟢 LONG",
+        "price": 100000,
+        "ema": 99999
     }
