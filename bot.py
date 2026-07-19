0840fcf50d14dd5ff83598ac5f5f9a0222acdf49
alzhwr795-ux <alzhwr795@gmail.com>
2026-07-19 10:57:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,30 @@
 import os
 import requests
+from analysis import analyze_btc
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-message = """🚀 إشارة تجريبية
+result = analyze_btc()
+
+message = f"""
+🚀 توصية تجريبية
 
 العملة: BTCUSDT
-النوع: LONG
-الدخول: 100000
-الهدف: 101000
-وقف الخسارة: 99500
+
+النوع: {result['signal']}
+
+السعر الحالي: {result['price']}
+
+EMA20: {result['ema']}
 """
 
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-requests.post(url, data={
-    "chat_id": CHAT_ID,
-    "text": message
-})
-
-print("Signal sent!")
+requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
